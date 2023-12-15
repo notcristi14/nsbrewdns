0405d02fd65fd6c4f4357683409bf8c82f2c3dc1
notcristi14 <153899201+notcristi14@users.noreply.github.com>
2023-12-15 11:10:41 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+import socket
+import time
+
+print('Getting Homebrew DNS');
+
+DNS = socket.gethostbyname('homebrew.heartium0.repl.co')
+
+
+print('Your DNS Adress Is: ' + DNS)
+
+print('Automatically closing in 60 seconds')
+
+time.sleep(60)
